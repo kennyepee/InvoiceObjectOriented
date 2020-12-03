5bccc3ffeb8cc17913729b54643a5308a5e97dec
Kenny Epee <kennyepee@Kennys-MacBook-Pro.local>
2020-12-03 17:55:14 -0500

    Initial commit

---
+++ src/com/company/Inventory.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Inventory {
+private String invproducts;
+private double invprices;
+
+
+
+
+    public String getInvproducts() {
+
+        return invproducts;
+    }
+
+    public void setInvproducts(String invproducts) {
+
+        this.invproducts = invproducts;
+    }
+
+    public double getInvprices() {
+
+        return invprices;
+    }
+
+    public void setInvprices(double price) {
+
+        this.invprices = price;
+    }
+
+}
+
+
---
+++ src/com/company/Purchases.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Purchases {
+private String purchproduct;
+private double purchprice;
+
+    public String getPurchproduct() {
+
+        return purchproduct;
+    }
+
+    public void setPurchproduct(String purchproduct) {
+
+        this.purchproduct = purchproduct;
+    }
+
+    public double getPurchprice() {
+        return purchprice;
+    }
+
+    public void setPurchprice(double purchprice) {
+
+        this.purchprice = purchprice;
+    }
+}
